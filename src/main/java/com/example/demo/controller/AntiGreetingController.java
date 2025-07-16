f54787c8b67bbac3702cd17124527e4abfaf307a
Young Seo Kim <youngseokim7@gmail.com>
2025-07-16 17:56:48 +0900

    setup anti-greeting route

--- src/main/java/com/example/demo/controller/AntiGreetingController.java
+++ src/main/java/com/example/demo/controller/AntiGreetingController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AntiGreetingController {
+    private String template = "fuck off ";
+    @GetMapping("/anti-greeting")
+    public AntiGreeting message(@RequestParam(value="name", defaultValue = "???") String name) {
+        return new AntiGreeting("from AntiGreetingController", template + name);
+    }
 }
